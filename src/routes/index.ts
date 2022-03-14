550584f48c9b2b5bea1fd5ca349c04cea7d087d0
creative201347 <creative201347@gmail.com>
2022-03-14 07:21:13 +0545

    create sessions

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,16 @@ import { Express } from "express";
 import { createUserHandler } from "../controllers/user.controller";
 import { createUserSchema } from "../schema/user.schema";
 import validateResource from "../middleware/validateResource";
+import { createSessionSchema } from "../schema/session.schema";
+import { createUserSessionHandler } from "../controllers/session.controller";
 
 function routes(app: Express) {
   app.post("/api/users", validateResource(createUserSchema), createUserHandler);
+  app.post(
+    "/api/sessions",
+    validateResource(createSessionSchema),
+    createUserSessionHandler
+  );
 }
 
 export default routes;
--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -19,14 +19,11 @@ export async function validatePassword({
   password: string;
 }) {
   const user = await UserModel.findOne({ email });
-
   if (!user) {
     return false;
   }
 
   const isValid = await user.comparePassword(password);
-
   if (!isValid) return false;
-
   return omit(user.toJSON(), "password");
 }
