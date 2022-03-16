55de9070be7876f73dcfe00924b019847c68294f
creative201347 <creative201347@gmail.com>
2022-03-16 06:39:33 +0545

    CRUD products

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,6 +9,18 @@ import {
   getUserSessionsHandler,
 } from "../controllers/session.controller";
 import requireUser from "../middleware/requireUser";
+import {
+  createProductSchema,
+  deleteProductSchema,
+  getProductSchema,
+  updateProductSchema,
+} from "../schema/product.schema";
+import {
+  createProductHandler,
+  deleteProductHandler,
+  getProductHandler,
+  updateProductHandler,
+} from "../controllers/product.controller";
 
 function routes(app: Express) {
   app.post("/api/users", validateResource(createUserSchema), createUserHandler);
@@ -19,6 +31,29 @@ function routes(app: Express) {
   );
   app.get("/api/sessions", requireUser, getUserSessionsHandler);
   app.delete("/api/sessions", requireUser, deleteSessionsHandler);
+
+  app.post(
+    "/api/products",
+    [requireUser, validateResource(createProductSchema)],
+    createProductHandler
+  );
+
+  app.get(
+    "/api/products/:productId",
+    validateResource(getProductSchema),
+    getProductHandler
+  );
+  app.put(
+    "/api/products/:productId",
+    [requireUser, validateResource(updateProductSchema)],
+    updateProductHandler
+  );
+
+  app.delete(
+    "/api/products/:productId",
+    [requireUser, validateResource(deleteProductSchema)],
+    deleteProductHandler
+  );
 }
 
 export default routes;
